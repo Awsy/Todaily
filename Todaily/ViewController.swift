74113773bcb89ea34609fbb8993a0ecd8592df18
Awsy <bass_master89@mail.ru>
2019-03-18 13:43:38 +0200

    Initial Commit

---
+++ Todaily/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todaily
+//
+//  Created by Aws Shkara on 3/18/19.
+//  Copyright © 2019 Aws Shkara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
